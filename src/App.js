b5135ddd50df865dfc8ff953e9ce89878f045e17
adarsh <adarshp2446018@gmail.com>
2024-08-07 15:58:16 +0530

    added the edit function

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>} />   
         <Route path="/register" element={<Register/>}/>
+        
       </Routes>
     </Router>
     
