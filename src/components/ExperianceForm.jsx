d8506258aba7c6ec0b9a01f42e548df6ea696060
darsh <darsh.s@enxcl.com>
2024-08-09 16:44:45 +0530

    CHANGES DONE

--- src/components/ExperianceForm.jsx
+++ src/components/ExperianceForm.jsx
@@ -246,7 +246,7 @@ function ExperianceForm() {
         <div>
             <ToastContainer />
             <form onSubmit={handleSubmit} className='formfull'>
-                <div className='formitem1'>
+                <div className='formydata'>
                     <div className='inneritems'>
                         <div className={sty.form_group}>
                             <input
